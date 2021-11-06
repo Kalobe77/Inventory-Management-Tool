9d63da52f9757be0d4467658fbc6aee0a7919148
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-11-06 12:51:21 -0400

    added models to admin panel and modified models slightly to give appealing UI in admin interface. also created superuser with username: webventory and password: webventory for testing purposes

--- webventory/home/admin.py
+++ webventory/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import User,Item,ItemHistory
+
+admin.site.register(Item)
+admin.site.register(User)
+admin.site.register(ItemHistory)
--- webventory/home/models.py
+++ webventory/home/models.py
@@ -10,11 +10,17 @@ class Item(models.Model):
     price = models.FloatField()
     user_visability = models.CharField(max_length=30)
 
+    def __str__(self):
+        return str(self.name) + ' (' + str(self.id) + ')'
+
 
 class User(models.Model):
     user_name = models.CharField(max_length=30)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.user_name
 
 
 class ItemHistory(models.Model):
@@ -26,3 +32,7 @@ class ItemHistory(models.Model):
     )
     change = models.IntegerField()
     date_of_change = models.DateField()
+
+    def __str__(self):
+        return " ".join([str(self.item_id.name), "Change:", str(self.change), "on", str(self.date_of_change.month) + "/" + str(self.date_of_change.day) + "/" + str(self.date_of_change.year)])
+
